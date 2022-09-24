a1f917467c77562db0f8cb02ac9923f8641e89f5
resuluygun <resuluygunn@gmail.com>
2022-09-24 22:29:46 +0300

    add db connection files

--- src/main/java/com/example/JDBCExecutor.java
+++ src/main/java/com/example/JDBCExecutor.java
@@ -1,7 +1,29 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCExecutor {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
+                    "hplussport",
+                    "postgres",
+                    "password");
+        try {
+
+            Connection connection = dcm.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select count(1) from customer");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1));
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
